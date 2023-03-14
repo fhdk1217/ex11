89a3f734ec386b88eb5549596829c68ffcf8405f
fhdk1217 <fhdk1217@naver.com>
2023-03-14 09:19:12 +0900

    ver03

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>리액트 시작</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>리액트 시작</h1>
+        </div>
+    );
 }
 
 export default App;
